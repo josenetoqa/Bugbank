9979728641c565e3dbe1b7e16b915b28c6c27868
José Neto <joseneto.2762@aluno.una.br>
2023-07-25 22:53:27 -0300

    feat : best report on cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,16 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     baseUrl: 'https://bugbank.netlify.app/',
     experimentalModifyObstructiveThirdPartyCode: true,
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+      require('cypress-terminal-report/src/installLogsPrinter')(on);
+},
+
   },
-    fixturesFolder: false,
-    video: true,
-})
+  fixturesFolder: false,
+  video:true,
+});
